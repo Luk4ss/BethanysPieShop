76d1b0f753c28cb6b42eb5b7c61e57e202ac458d
lucas <lucasregoaraujo@gmail.com>
2022-07-15 09:38:30 -0300

    Created Controller, View and ViewModel for Pie.

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,33 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieCategoryRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieCategoryRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        //ViewResult is a  built-in type of ASP.NET core MVC
+        public IActionResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+
+            piesListViewModel.Pies = _pieRepository.AllPies;
+            piesListViewModel.CurrentCategory = "Cheese Cakes";
+
+            return View(piesListViewModel);
+        }
+    }
+}
